b5dffb61011ebc4fdb22bd4632bbf7bdc54e05d1
Richard Myatt <lydgate77@hotmail.co.uk>
2018-06-09 18:38:32 +0100

    App now in draft.  List showing on home page and individual recipes displaying as expected.  Work now required on links.

--- js/recipe.js
+++ js/recipe.js
@@ -1,31 +1,31 @@
-var recipes = [{
+var myRecipes = { a: {
                   title: "Chicken in Spicy Tomato Sauce",
                   serves: 4,
                   ingredients: "4 chicken portions\n2 ounces butter/n/nFor the Sauce\n4 tablespoons tomato ketchup\n1 tablespoon onion; finely grated\n2 tablespoons water\n2 tablespoons wine vinegar\n2 teaspoons soft brown sugar\n0.5 teaspoon mustard powder\n0.5 teaspoon salt\n\nFor the Garnish\nbunch of watercress",
                   method: "Heat the grill to medium heat.\n\nMelt the butter in a small saucepan.  Brush the chicken joints all over with the melted butter.\n\nRemove the grid from the grill pan and arrange the chicken pieces, skin side down and side by side, in the bottom of the grill pan, or in a large flame-proof gratin dish.\n\nGrill the chicken 5 inches away from the heat for 5 minutes.  Turn the chicken over and grill for a further 5 minutes.\n\nMeanwhile grate the onion over a plate to catch the juice.\n\nPut the onion into the saucepan containing the remaining melted butter. Stir in the other sauce ingredients and simmer for 5 minutes.\n\nBrush the chicken with the sauce and continue grilling under moderate heat, turning and brushing with more sauce every 5 minutes until the chicken is cooked right through, a total of 30 minutes.\n\nIf the grill pan was used, turn the chicken pieces onto a hot serving dish and spoon the sauce on top.  Garnish with watercress."
               },
-              {
+              b: {
                 title: "Chow Mien",
                 serves: 4,
                 ingredients: "1 pound egg noodles\n10 ounces pork fillet\n2 teaspoons corn flour\n4 ounces bamboo shoots\n4 ounces spinach leaves\n0.5 cucumber\n5 tablespoons oil\n\nSauce\n2 tablespoons soy sauce\n1 tablespoon sherry\n1 teaspoon salt\n1 teaspoon sugar\n1 teaspoon cornflour\n1 teaspoon sesame seed oil, to garnish",
                 method: "Bring a saucepan of water to the boil, add the noodles and simmer for five minutes until soft but not sticky.  Drain in a sieve and rinse with cold water.\n\nCut the pork into shreds the size of a match-stick and mix them with the cornflour.\n\nCut the bamboo shoots, spinach and cumumber into thin shreds the same size as the pork.\n\nHeat about half the oil in a wok or frying pan.  Place the noodles in a large bowl, separating them with a fork, then pour over the hot oil.\n\nSiir to ensure the noodles are evenly coated.  Return them to the wok or pan and stir fry for 2 to 3 minutes.  Remove them with a perforated spoon and place on a serving dish.\n\nHeat the remaining oil in the pan and stir fry the bamboo shoots, cucumber, spinach, port and cornflour.  Mix together the suace ingredients and pour into the pan.  Cook for about 2 minutes, then pour the mixture over the noodles, garnish with sesame seed oil and serve."
               },
-              {
+              c: {
                 title: "Drop scones",
                 serves: 30,
                 ingredients: "5 oz plain flour\n1 large pinch of salt\n2 tsps baking powder\n2.5 tsps caster sugar\n1 egg\n1 oz butter; melted\n0.25 pt milk",
                 method: "Heat the griddle or heavy based frying pan over a moderate heat.  Sift the flour, salt and baking powder into a large bow. Add the sugar.  Make a well in the centre and drop in the egg and melted butter.  Gradually add the milk, mixing with a wooden spoon to give a quick smooth batter.\n\nLightly grease the griddle or pan and pour on little pools of the mixture to give perfectly round cakes.  As soon as the cakes are puffed and full of bubbles and the underside brown, flip them over with a palette knife to brown the other side.  Serve warm."
               },
-              {
+              d: {
                 title: "Egg Fried Rice",
                 serves: 2,
                 ingredients: "14 ounces pre-cooked long grain rice; refrigerated\n4 teaspoons oyster sauce\n2 teaspoons soy sauce\n2 eggs; beaten\n4 tablespoons peanut oil\n2 tablespoons carrots; shredded\n4 tablespoons frozen peas; thawed\n1 spring onion; sliced\n2 cloves garlic; minced\nsalt and pepper",
                 method: "Add half the oil to a hot wok and fry the eggs and stir to break it up.\n\nAdd remaining oil to the wok and introduce the garlic and the rice.  Stir until all is mixed together.\n\nAdd the peas carrots and spring onions.  Mix together until all the flavours have blended.\n\nAdd the oyster sauce and the soy sauce and season with salt and pepper. Toss it all together and remove from the heat.\n\nServe in individual small bowls."
               },
-              {
+              e: {
                 title: "Flapjacks",
                 serves: 12,
                 ingredients: "6 ounces butter or margarine\n4 tablespoons golden syrup\n8 ounces Demarera sugar\n6 ounces whole oats\n6 ounces porridge oats",
                 method: "Heat oven to 150 degrees C .  Lightly brush an 6 x 12 in tin with melted fat or oil.\n\n Cut the fat into small pieces and place in a heavy-based saucepan.  Add the golden syrup and sugar and place saucepan over a low heat.\n\nMix the ingredients together, then stir, continuously, with a wooden spoon until the fat has melted and the sugar has just dissolved.\n\n Remove the pan from the heat and stir in the whole and porridge oats.  Mix together until the mixture is evenly combined.\n\n Turn the mixture into the prepared tin and smooth the surface flat with the back of a metal spoon.\n\nBake in the centre of the oven for 40 minutes, until the surface is golden brown.\n\nPlace the tin on a wooden board and, using a sharp knife, cut the biscuit into 10-12 shapes.  Leave the biscuits in the tin until completely cold before removing."
               }
-];
+};
--- js/recipe_box.js
+++ js/recipe_box.js
@@ -1,7 +1,37 @@
+
+var TitleBar = {
+  template: "#logo"
+};
+
+var RecipeList = {
+  template: "#list",
+  props: ["db"]
+};
+
+var Recipe = {
+  template: "#recipe",
+  props: ["db"]
+};
+
+// define the routes
+var routes = [
+  {path: "/", component: RecipeList},
+  {path: "/recipe/:id", component: Recipe}
+];
+
+var router = new VueRouter({
+  routes: routes
+});
+
 var main = new Vue({
   el: "#app",
   data: {
-    result: "good",
-    db: recipes
+    db: myRecipes
+  },
+  router: router,
+  components: {
+    logo: TitleBar,
+    recipe: Recipe,
+    "recipe-list": RecipeList
   }
 });
